716d52b3ab6b19bcece5c96ce662a33c21e0d7af
Zhihan <zhihan.fang@rutgers.edu>
2015-06-20 20:19:52 +0800

    modify all product view

--- src/sql/view/create_all_products_steps_view.sql
+++ src/sql/view/create_all_products_steps_view.sql
@@ -1,8 +1,9 @@
 CREATE VIEW all_products_steps_view AS
-SELECT library_casestep.id, library_casestep.instruction, library_casestep.expected,
+SELECT library_case.id as case_id,library_casestep.id as step_id, library_casestep.instruction, library_casestep.expected,
 	   library_caseversion.name as version_name,library_caseversion.description,library_caseversion.status,
 	   core_productversion.version,core_product.name  as product_name
-FROM  library_casestep,library_caseversion,core_productversion,core_product
+FROM  library_casestep,library_caseversion,library_case,core_productversion,core_product
 WHERE library_casestep.caseversion_id = library_caseversion.id
   and library_caseversion.productversion_id = core_productversion.id
   and core_productversion.product_id = core_product.id
+  and library_caseversion.case_id = library_case.id
